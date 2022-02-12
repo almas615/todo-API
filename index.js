ca71cdf04d3fa9fb0e8ef2ba4062669368dbbfc7
Almas Adlil Wafi <almasadlilwafi615@gmail.com>
2022-02-12 08:24:22 +0700

    install cores

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 // eslint-disable-next-line import/extensions
 const router = require('./routes');
 require('dotenv').config();
@@ -7,6 +8,7 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
